ebfed6d0b12c1b28f6d3d680b677aeca06a3fd0d
Youyang Shen <74205353+YouyangShen@users.noreply.github.com>
2021-01-04 20:20:47 +0100

    Add files via upload

--- exercise 1.2/exercise2.m
+++ exercise 1.2/exercise2.m
@@ -1,6 +1,6 @@
 format long
 
-%% cm
+%% 3.1 
 D = 6.5;
 n = 1;
 C = 2000;
@@ -14,3 +14,18 @@ L=1;
 b0=0.26;
 
 [Edx,Ebx,Edy,Eby] = EdEb(Xcw,Ycw,Xccw,Yccw,L,b0);
+
+%% 3.2
+L =2;
+Xcw1 = 0.266;
+Ycw1 = 2.217-2;
+Xccw1 = 0.266;
+Yccw1 = 1.783-2;
+[Edx,Ebx,Edy,Eby] = EdEb(Xcw1,Ycw1,Xccw1,Yccw1,L,b0);
+
+%% 3.3
+Xcw2 = -0.010;
+Ycw2 = 0.009;
+Xccw2 = 1.986-2;
+Yccw2 = 2.014-2;
+
